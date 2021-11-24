bf19d1ae43143e131d70d9761b05ea0ed7028512
Akash Sharma <89353868+AkashFlix@users.noreply.github.com>
2021-11-24 16:53:02 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,17 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , \nI'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail And Custom Caption support!</b> \n
+    START_TEXT = """ <b> Hɪɪ {} , \\nI'ᴍ ᴀ ʀᴇɴᴀᴍᴇ ʙᴏᴛ. 😎
 
-<b>Bot Maintained By: @MaI_BoTs </b> \n 
+Aɴᴅ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ Fɪʟᴇs ᴛᴏ Vɪᴅᴇᴏ ᴡɪᴛʜ Pᴇʀᴍᴀɴᴇɴᴛ Tʜᴜᴍʙɴᴀɪʟ ᴀɴᴅ Cᴜsᴛᴏᴍ Cᴀᴘᴛɪᴏɴ Sᴜᴘᴘᴏʀᴛ. 🤫
+
+Jᴜsᴛ sᴇɴᴅ ᴍᴇ Fɪʟᴇs/Vɪᴅᴇᴏ ᴀɴᴅ sᴇᴇ ᴛʜᴇ Mᴀɢɪᴄ. 🪄</b> \n
+
+<b>Bʏ - @Bot_Flix. </b> \n 
 """
 
     BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
     DOWNLOAD_START = "<b>Downloading To My server !! Pls Wait</b>"
     UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TeleGram</b>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @MaI_BoTs ❤️</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me Support Our Channel @Anime_Galaxyz ❤️</b>"
     SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
     DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
     SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
@@ -17,7 +21,7 @@ class Translation(object):
     NO_THUMB_FOUND = "No Thumbnail found"
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
     ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @No_OnE_Kn0wS_Me If You Found Meh Useful</b>"""
-    HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @MaI_BoTs If You Find This Bot Usefull❤️"""
+    HELP_USER = """Checkout The Available Commands Here \n\n Do Follow @Anime_Galaxyz If You Find This Bot Usefull❤️"""
     RENAME_HELP = """Here are The Available Commands In Reanme \n\n\n▪️ <code>/rename</code> : Reply To An File/video With <code>/rename Filename.extension</code> For Renaming"""
     C2V_HELP = """Here Are The Available Commands In File To Video \n\n\n ▪️<code>/c2v</code> : Reply To An File With /c2v To Convert It Into Video"""
     THUMBNAIL_HELP = """Here Are The Available Commands In Custom Thumbnail \n\n\n ▪️ Send A Photo To Set The Custom Thumbnail \n▪️ <code>/showthumb</code> : For Checking The Current Thumbnail \n▪️<code>/delthumb</code> : For Deleting The Current Saved Thumbnail"""
